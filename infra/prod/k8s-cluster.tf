d7b76ff9f41efa99b994d149708f5d8ca8cbaf62
roshen-ideabits <roshen@ideabits.se>
2025-05-24 21:00:21 +0530

    added terraform scripts to create vpc and kubernetes cluster

---
+++ infra/prod/k8s-cluster.tf
@@ -0,0 +1,20 @@
+resource "digitalocean_kubernetes_cluster" "prod" {
+  name           = "personal-website-prod-cluster"
+  region         = "sgp1"
+  version        = "1.32.2-do.1"
+  vpc_uuid       = digitalocean_vpc.prod.id
+  cluster_subnet = "10.108.0.0/16"
+  service_subnet = "10.109.0.0/19"
+  ha             = false
+
+  node_pool {
+    name       = "personal-website-prod-pool"
+    size       = "s-1vcpu-1gb"
+    node_count = 2
+  }
+
+  maintenance_policy {
+    day        = "sunday"
+    start_time = "02:00"
+  }
+}
